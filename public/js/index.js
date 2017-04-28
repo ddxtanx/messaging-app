3691548abbfb979086758c491ab097c80ae98701
Garrett Credi <gcc@ameritech.net>
2017-04-28 23:54:02 +0000

    Add features, messaging when a user connects or disconnects, and prohibiting users having the same name as a previous user

--- public/js/index.js
+++ public/js/index.js
@@ -13,24 +13,59 @@ function randInt(max){
     return Math.floor(Math.random()*max);
 }
 var userName = "";
-while (userName==""){
-    userName=prompt("What do you want your name to be?");
-}
+var userNames = [];
 var HOST = window.location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(HOST);
 var messageId = 0;
+ws.onopen = function(){
+    var openData = {
+        request: 'names'
+    };
+    ws.send(JSON.stringify(openData));
+};
 ws.onmessage = function(e){
+    console.log(e);
     var data = e.data;
     data = JSON.parse(data);
     if(data.type=="message"){
+        console.log("RECEIVED MESSAGE");
         if(data.id!=messageId){
             addMessage(data.message, 0, data.from);
         }
     }else if(data.type=="conn"){
-        console.log("connection added")
+        console.log("connection added");
+        var connected = data.usersConnected;
+        if(data.name!==userName){
+            userNames.push(data.name);
+        }
+        var noun = (connected!=1)?"Users":"User";
+        $("#usersH2").text(connected+" "+noun+" Connected");
+        addMessage(data.name+" Has Connected", false, "SERVER");
+    } else if(data.type=="close"){
+        console.log("RECEIVED CLOSE");
         var connected = data.usersConnected;
         var noun = (connected!=1)?"Users":"User";
         $("#usersH2").text(connected+" "+noun+" Connected");
+        addMessage(data.name+" Has Disconnected", false, "SERVER");
+    } else if(data.type=="closeRequest"){
+        var closeResData = {
+            name: userName,
+            type: 'closeRes'
+        };
+        console.log("RECEIVED CLOSE REQUEST "+JSON.stringify(closeResData));
+        ws.send(JSON.stringify(closeResData));
+    } else if(data.type=="names"){
+        userNames = data.names;
+        while ((userName=="" || !(userNames.indexOf(userName)==-1)) || userName==""){
+            userName=prompt("What do you want your name to be?");
+            console.log("USERNAME: "+userName+"\n INDEX: "+userNames.indexOf(userName));
+        }
+        console.log(userNames.indexOf(userName));
+        var connData = {
+            type: "conn",
+            name: userName
+        };
+        ws.send(JSON.stringify(connData));
     }
 };
 $(document).ready(function(){
@@ -62,4 +97,4 @@ $(document).ready(function(){
             }
         }
     });
-})
+});
--- server.js
+++ server.js
@@ -5,44 +5,89 @@ var express = require('express'),
     http = require('http');
 var server = http.createServer(app);
 var wss = new SocketServer({server});
+var connections = 0;
+var userNames = [];
+var userNamesCopy = [];
+var requestsIndex = 0;
+var webSockets = {};
+function randInt(min, max){
+    return Math.floor(Math.random()*(max-min))+min
+}
 app.set('views', './public');
 app.use(express.static('./public'), bodyParser());
 
 app.get("/", function(req, res){
+    console.log(userNames);
     res.render("twig/index.twig");
 });
-var connections = 0;
 wss.on('connection', function(ws){
     connections++;
     console.log("connecton");
-    wss.clients.forEach(function(client){
-        var data = {
-            type: 'conn',
-            usersConnected: connections
-        };
-        client.send(JSON.stringify(data));
-    });
+    webSockets[randInt(50,500)] = ws;
     ws.on('message', function(data){
         console.log("data "+data);
-        wss.clients.forEach(function(client){
-            console.log("sent to client");
-            if(!data.includes("\"type\": ")){
-                data = [data.slice(0, data.length-1), ", \"type\": \"message\"", data.slice(data.length-1)].join('')
-            }
-            console.log(data);
-            client.send(data);
-        });
+        if(JSON.parse(data).type=="closeRes"){
+            console.log("RECEIVED CLOSE RES");
+            userNamesCopy = userNamesCopy.filter(function(item){
+                return item!==JSON.parse(data).name;
+            });
+            requestsIndex++;
+        } else if (JSON.parse(data).request=="names"){
+            ws.send(JSON.stringify({
+                type: "names",
+                names: userNames
+            }));
+            console.log("Sending names");
+        }else {
+            wss.clients.forEach(function(client){
+                console.log("sent to client");
+                if(!data.includes("\"type\":")){
+                    data = [data.slice(0, data.length-1), ", \"type\": \"message\"", data.slice(data.length-1)].join('');
+                }
+                data = JSON.parse(data);
+                if(data.type=="conn"){
+                    if(userNames.indexOf(data.name)==-1){
+                        userNames.push(data.name);
+                        userNamesCopy = userNames;
+                        console.log(userNames+"???"+userNamesCopy);
+                    }
+                }
+                data.usersConnected = connections;
+                client.send(JSON.stringify(data));
+                data = JSON.stringify(data);
+            });
+        }
+        if(requestsIndex!=0&&requestsIndex==connections){
+            console.log("AT NUMBER");
+            var disconnectedUser = userNamesCopy[0];
+            console.log(userNamesCopy);
+            userNames = userNames.filter(function(name){
+                return name!==disconnectedUser;
+            });
+            console.log(userNamesCopy);
+            requestsIndex=0;
+            userNamesCopy = userNames;
+            wss.clients.forEach(function(client){
+                var closeResData = {
+                   type: 'close',
+                   usersConnected: connections,
+                   name: disconnectedUser
+               };
+               console.log(closeResData);
+               client.send(JSON.stringify(closeResData));
+            });
+        }
     });
-    ws.on('close',function(ws){
-        console.log('disconnect '+ws);
+    ws.on('close', function(closeData){
+        console.log("closed "+closeData);
         connections--;
         wss.clients.forEach(function(client){
-        var data = {
-            type: 'conn',
-            usersConnected: connections
-        };
-        client.send(JSON.stringify(data));
-    });
+            var closeData = {
+                usersConnected: connections,
+                type: 'closeRequest'
+            };
+            client.send(JSON.stringify(closeData));
+        });
     });
 });
 server.listen(process.env.PORT || 8080);
